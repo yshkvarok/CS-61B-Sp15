ae51e5175ff6639d1d5158e5e2d26949499e904f
yshkvarok <yuriy.shkvarok16@my.stjohns.edu>
2017-07-10 16:08:25 -0400

    Completed lecture 1/30/15 classwork

---
+++ lecture2015-01-30/FloatingPoint.java
@@ -0,0 +1,8 @@
+public class FloatingPoint {
+	public static void main(String[] args) {
+		System.out.println(0.1);
+		System.out.println(0.2);
+		System.out.println(0.1 + 0.2);
+		System.out.println(0.1 + 0.2 - 0.1);
+	}
+}
---
+++ lecture2015-01-30/Integers.java
@@ -0,0 +1,7 @@
+public class Integers {
+	public static void main(String[] args) {
+		System.out.println(2147483647 + 1);
+		System.out.println(1000000000 * 5);
+		System.out.println(29 & 15);
+	}
+}
