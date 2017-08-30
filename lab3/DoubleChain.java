ab599d9a52be876897e854d608e10f295cb094ad
yshkvarok <yuriy.shkvarok16@my.stjohns.edu>
2017-08-30 01:47:33 -0400

    Finished Lab 3

--- lab3/DoubleChain.java
+++ lab3/DoubleChain.java
@@ -4,8 +4,7 @@ public class DoubleChain {
 	private DNode head;
 	
 	public DoubleChain(double val) {
-		/* your code here. */
-		head = null; 
+		head = new DNode(val);
 	}
 
 	public DNode getFront() {
@@ -14,32 +13,81 @@ public class DoubleChain {
 
 	/** Returns the last item in the DoubleChain. */		
 	public DNode getBack() {
-		/* your code here */
-		return null;
+		DNode backNode = head;
+
+		while (backNode.next != null) {
+			backNode = backNode.next;
+		}
+		return backNode;
 	}
 	
 	/** Adds D to the front of the DoubleChain. */	
 	public void insertFront(double d) {
-		/* your code here */
+		DNode newHead = new DNode(d);
+		newHead.next = head;
+		head.prev = newHead;
+		head = newHead;
 	}
 	
 	/** Adds D to the back of the DoubleChain. */	
 	public void insertBack(double d) {
-		/* your code here */
+		DNode backNode = this.getBack();
+		DNode newNode = new DNode(d);
+
+		backNode.next = newNode;
+		newNode.prev = backNode;
 	}
 	
 	/** Removes the last item in the DoubleChain and returns it. 
 	  * This is an extra challenge problem. */
 	public DNode deleteBack() {
-		/* your code here */
-		return null;
+		DNode backNode = head;
+
+		while (backNode.next != null) {
+			backNode = backNode.next;
+		}
+
+		backNode.prev.next = null;
+		backNode.prev = null;
+
+		return backNode;
+	}
+
+	public DNode deleteByIndex(int i) {
+		DNode iNode = head;
+
+		if (i == 0) {
+			iNode.next.prev = null;
+			head = iNode.next;
+			return iNode;
+		}
+
+		for (int index = 0; index < i; index++)
+			iNode = iNode.next;
+
+		if (iNode.next != null)
+			iNode.next.prev = iNode.prev;
+
+		iNode.prev.next = iNode.next;
+
+		return iNode;
 	}
 	
 	/** Returns a string representation of the DoubleChain. 
 	  * This is an extra challenge problem. */
 	public String toString() {
-		/* your code here */		
-		return null;
+		String list = "<[";
+
+		DNode iter = head;
+		list += iter.val;
+
+		while (iter.next != null) {
+			iter = iter.next;
+			list += ", " + iter.val;
+		}
+		list += "]>";
+
+		return list;
 	}
 
 	public static class DNode {
@@ -54,7 +102,13 @@ public class DoubleChain {
 		private DNode(DNode prev, double val, DNode next) {
 			this.prev = prev;
 			this.val = val;
-			this.next =next;
+			this.next = next;
+		}
+
+		/** Helper method for testing
+		  */
+		public double value() {
+			return val;
 		}
 	}
 	
--- lab3/TestDoubleChain.java
+++ lab3/TestDoubleChain.java
@@ -31,6 +31,41 @@ public class TestDoubleChain {
         assertEquals(8, d.getBack().val, 1e-11);
     }
 
+    @Test
+    public void testToString() {
+        DoubleChain d = new DoubleChain(1);
+        d.insertBack(2.1);
+        d.insertBack(3.3);
+        d.insertBack(4.6);
+        d.insertBack(5.2);
+
+        assertEquals("<[1.0, 2.1, 3.3, 4.6, 5.2]>", d.toString());
+    }
+
+    @Test
+    public void testDeleteBack() {
+        DoubleChain d = new DoubleChain(1);
+        d.insertBack(2.1);
+        d.insertBack(3.3);
+        d.insertBack(4.6);
+        d.insertBack(5.2);
+        d.deleteBack();
+
+        assertEquals(4.6, d.getBack().value(), 0.0f);
+    }
+
+    @Test
+    public void testDeleteByIndex() {
+        DoubleChain d = new DoubleChain(3);
+        d.insertBack(2.1);
+        d.insertBack(3.3);
+        d.insertBack(4.6);
+        d.insertBack(5.2);
+        d.deleteByIndex(4);
+
+        assertEquals("<[3.0, 2.1, 3.3, 4.6]>", d.toString());
+    }
+
     public static void main(String[] args) {
         jh61b.junit.textui.runClasses(TestDoubleChain.class);
     }
