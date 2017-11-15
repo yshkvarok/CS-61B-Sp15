cf7bde950ad928269a5d060c5882dbc5ccd8c557
Yuriy Shkvarok <yshkvarok@gmail.com>
2017-11-15 15:20:08 -0500

    HW #5 parts 1-4 completed

--- hw5/ULLMap.java
+++ hw5/ULLMap.java
@@ -1,4 +1,6 @@
 import java.util.Set; /* java.util.Set needed only for challenge problem. */
+import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 /** A data structure that uses a linked list to store pairs of keys and values.
  *  Any key must appear at most once in the dictionary, but values may appear multiple
@@ -8,39 +10,74 @@ import java.util.Set; /* java.util.Set needed only for challenge problem. */
  *  For simplicity, you may assume that nobody ever inserts a null key or value
  *  into your map.
  */ 
-public class ULLMap { //FIX ME
+public class ULLMap<K, V> implements Map61B<K, V>, Iterable<K> { //FIX ME
     /** Keys and values are stored in a linked list of Entry objects.
       * This variable stores the first pair in this linked list. You may
       * point this at a sentinel node, or use it as a the actual front item
       * of the linked list. 
       */
     private Entry front;
+    private int sz;
 
     @Override
-    public get(key) { //FIX ME
+    public V get(K key) { //FIX ME
     //FILL ME IN
-        return null; //FIX ME
+        return front.get(key).val; //FIX ME
     }
 
     @Override
-    public void put(key, val) { //FIX ME
-    //FILL ME IN
+    public void put(K key, V val) { //FIX ME
+        if (front == null) {
+            front = new Entry(key, val, null);
+            sz++;
+            return;
+        }
+        Entry ptr = front;
+        while (ptr.next != null) {
+            ptr = ptr.next;
+        }
+        ptr.next = new Entry(key, val, null);
+        sz++;
+    }
+
+    public String toString() {
+        Iterator<K> iter = new ULLMapIter();
+        String str = "";
+        while (iter.hasNext()) {
+            str += (Object) iter.next();
+        }
+        return str;
     }
 
     @Override
-    public boolean containsKey(key) { //FIX ME
+    public boolean containsKey(K key) { //FIX ME
     //FILL ME IN
-        return false; //FIX ME
+        if (front == null) return false;
+        return (front.get(key) != null); //FIX ME
     }
 
     @Override
     public int size() {
-        return 0; // FIX ME (you can add extra instance variables if you want)
+        return sz; // FIX ME (you can add extra instance variables if you want)
     }
 
     @Override
     public void clear() {
     //FILL ME IN
+        front = null;
+        sz = 0;
+    }
+
+    public static <K, V> ULLMap inverse(ULLMap<K, V> og) {
+        ULLMap<V, K> newMap = new ULLMap<V, K>();
+        Iterator<K> iter = og.iterator();
+        while (iter.hasNext()) {
+            K temp = iter.next();
+            if (!(newMap.containsKey(og.get(temp)))) {
+                newMap.put(og.get(temp), temp);
+            }
+        }
+        return newMap;
     }
 
 
@@ -50,7 +87,7 @@ public class ULLMap { //FIX ME
     
         /** Stores KEY as the key in this key-value pair, VAL as the value, and
          *  NEXT as the next node in the linked list. */
-        public Entry(k, v, Entry n) { //FIX ME
+        public Entry(K k, V v, Entry n) { //FIX ME
             key = k;
             val = v;
             next = n;
@@ -58,34 +95,76 @@ public class ULLMap { //FIX ME
 
         /** Returns the Entry in this linked list of key-value pairs whose key
          *  is equal to KEY, or null if no such Entry exists. */
-        public Entry get(k) { //FIX ME
+        public Entry get(K k) { //FIX ME
             //FILL ME IN (using equals, not ==)
+            Entry ptr = front;
+            while (ptr != null) {
+                if (k.equals(ptr.key)) {
+                    return ptr;
+                }
+                ptr = ptr.next;
+            }
+
             return null; //FIX ME
         }
 
         /** Stores the key of the key-value pair of this node in the list. */
-        private key; //FIX ME
+        private K key; //FIX ME
         /** Stores the value of the key-value pair of this node in the list. */
-        private val; //FIX ME
+        private V val; //FIX ME
         /** Stores the next Entry in the linked list. */
         private Entry next;
     
     }
 
+    public class ULLMapIter implements Iterator<K> {
+        public ULLMapIter() {
+            iter = front;
+            index = 0;
+        }
+
+        public boolean hasNext() {
+            return index < sz;
+        }
+
+        public K next() {
+            K temp;
+            if (!hasNext()) throw new NoSuchElementException();
+            temp = iter.key;
+            index++;
+            if (iter.next != null) {
+                iter = iter.next;
+            }
+            return temp;
+        }
+
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+
+        /** Position of Iterator variable. **/
+        private Entry iter;
+        private int index;
+    }
+
+    public Iterator<K> iterator() {
+        return new ULLMapIter();
+    }
+
     /* Methods below are all challenge problems. Will not be graded in any way. 
      * Autograder will not test these. */
     @Override
-    public remove(key) { //FIX ME SO I COMPILE
+    public V remove(K key) { //FIX ME SO I COMPILE
         throw new UnsupportedOperationException();
     }
 
     @Override
-    public remove(key, value) { //FIX ME SO I COMPILE
+    public V remove(K key, V value) { //FIX ME SO I COMPILE
         throw new UnsupportedOperationException();
     }
 
     @Override
-    public Set<> keySet() { //FIX ME SO I COMPILE
+    public Set<K> keySet() { //FIX ME SO I COMPILE
         throw new UnsupportedOperationException();
     }
 
--- hw5/ULLMapTest.java
+++ hw5/ULLMapTest.java
@@ -8,22 +8,75 @@ import java.util.Iterator;
 
 public class ULLMapTest {
     @Test
+    public void testGet() {
+    	ULLMap<String, String> um = new ULLMap<String, String>();
+        um.put("Gracias", "Dios Basado");
+        um.put("Gracias1", "Dios Basado1");
+        um.put("Gracias2", "Dios Basado2");
+        um.put("Gracias3", "Dios Basado3");
+        assertEquals(4, um.size());
+        assertEquals(true, um.containsKey("Gracias3"));
+        um.clear();
+        assertEquals(0, um.size());
+        assertEquals(false, um.containsKey("Gracias3"));
+    }
+
+    @Test
+    public void testContainsKey() {
+    	ULLMap<Integer, String> um = new ULLMap<Integer, String>();
+    	um.put(1, "Hi");
+    	um.put(2, "Hello");
+    	um.put(3, "How are you");
+    	assertTrue(um.containsKey(3));
+    }
+
+    @Test
     public void testBasic() {
         ULLMap<String, String> um = new ULLMap<String, String>();
         um.put("Gracias", "Dios Basado");
+        um.put("Black", "Boi");
         assertEquals(um.get("Gracias"), "Dios Basado");
+        assertEquals(um.get("Black"), "Boi");
     }
 
+    @Test
+    public void testToString() {
+    	ULLMap<Integer, String> um = new ULLMap<Integer, String>();
+        um.put(5, "Dios Basado");
+        um.put(6, "Boi");
+        um.put(7, "WAT Up");
+        um.put(8, "yea boi");
+        um.put(9, "asas");
+        um.put(1, "aaa");
+
+        assertEquals("567891", um.toString());
+
+   		Iterator<Integer> iter1 = um.iterator();
+   		assertEquals(5, (int) iter1.next());
+   		assertEquals(6, (int) iter1.next());
+   		assertEquals(7, (int) iter1.next());
+    }
     
-    /*@Test
-    public void testIterator() {
+    @Test
+    public void testGet2() {
         ULLMap<Integer, String> um = new ULLMap<Integer, String>();
         um.put(0, "zero");
         um.put(1, "one");
         um.put(2, "two");
-        Iterator<Integer> umi = um.iterator();
-        System.out.println(umi.next());
-    }*/
+        
+        assertEquals("zero", um.get(0));
+    }
+
+    @Test
+    public <K, V> void testInvert() {
+    	ULLMap<Integer, String> um = new ULLMap<Integer, String>();
+    	um.put(0, "zero");
+        um.put(1, "one");
+        um.put(2, "two");
+
+        ULLMap newMap = ULLMap.inverse(um);
+        assertEquals("zeroonetwo", newMap.toString());
+    }
     
 
     /** Runs tests. */
